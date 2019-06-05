6cffe7fd8047891690afa3b0ae0e5b63cf426a67
- <->
2019-06-05 14:30:14 +0800

    .

--- lib/ui/util/menu.dart
+++ lib/ui/util/menu.dart
@@ -3,28 +3,15 @@ import 'package:flutter/services.dart';
 
 class Menu extends StatefulWidget {
   @override
-  _MenuState createState() => _MenuState(now);
-  final int now;
+  _MenuState createState() => _MenuState();
 
-  Menu(int now, {Key key})
-      : this.now = now,
-        super(key: key);
+  Menu({Key key}) : super(key: key);
 }
 
 class _MenuState extends State<Menu> {
   final padding = 6.0;
   int now;
-  _MenuState(int now) : this.now = now;
-  bool _handleKeyPress(FocusNode node, RawKeyEvent event) {
-    if (event is RawKeyDownEvent) {
-      if (event.logicalKey == LogicalKeyboardKey(0x10200000017) ||
-          event.logicalKey == LogicalKeyboardKey.enter) {
-        print("\n_MenuState $event");
-        return true;
-      }
-    }
-    return false;
-  }
+  _MenuState(int now) : this.now = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -42,52 +29,60 @@ class _MenuState extends State<Menu> {
     for (int position = 0; position < li.length; ++position) {
       Widget focus = Focus(
           //                skipTraversal: this.widget.now == position,
-          onKey: _handleKeyPress,
-          child: Builder(builder: (BuildContext context) {
-            final FocusNode focusNode = Focus.of(context);
-            final bool hasFocus = focusNode.hasFocus;
-            BoxDecoration decoration;
-            Color color;
-            if (hasFocus) {
-              if (this.now == position) {
-                color = Colors.orange;
-              } else
-                color = Colors.yellow;
-            } else {
-              color = Colors.grey;
-            }
-            if (position == this.now) {
-              decoration = BoxDecoration(
-                  border: Border(
-                      bottom: BorderSide(
-                color: color,
-                width: 1.4,
-              )));
-            }
+          onKey: (FocusNode node, RawKeyEvent event) {
+        if (event is RawKeyDownEvent) {
+          if (event.logicalKey == LogicalKeyboardKey(0x10200000017) ||
+              event.logicalKey == LogicalKeyboardKey.enter) {
+            print("\n_MenuState $event $node");
+            this.now = position;
+            return true;
+          }
+        }
+        return false;
+      }, child: Builder(builder: (BuildContext context) {
+        final FocusNode focusNode = Focus.of(context);
+        final bool hasFocus = focusNode.hasFocus;
+        BoxDecoration decoration;
+        Color color;
+        if (hasFocus) {
+          if (this.now == position) {
+            color = Colors.orange;
+          } else
+            color = Colors.yellow;
+        } else {
+          color = Colors.grey;
+        }
+        if (position == this.now) {
+          decoration = BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(
+            color: color,
+            width: 1.4,
+          )));
+        }
 
-            return GestureDetector(
-                onTap: () {
-                  focusNode.requestFocus();
-                  setState(() {
-                    this.now = position;
-                  });
-                },
-                child: Padding(
-                    child: Container(
-                        decoration: decoration,
-                        child: Padding(
-                          padding: EdgeInsets.only(bottom: padding / 2),
-                          child: Text(
-                            li[position],
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: color,
-                            ),
-                          ),
-                        )),
-                    padding:
-                        EdgeInsets.fromLTRB(padding, padding, padding, 0)));
-          }));
+        return GestureDetector(
+            onTap: () {
+              focusNode.requestFocus();
+              setState(() {
+                this.now = position;
+              });
+            },
+            child: Padding(
+                child: Container(
+                    decoration: decoration,
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: padding / 2),
+                      child: Text(
+                        li[position],
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: color,
+                        ),
+                      ),
+                    )),
+                padding: EdgeInsets.fromLTRB(padding, padding, padding, 0)));
+      }));
 
       if (position == 5) {
         t = right;
