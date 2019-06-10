c2e05fe764a587e14059d42da8a7b11d886302d3
- <->
2019-06-10 16:10:48 +0800

    .

--- lib/ui/util/video/list.dart
+++ lib/ui/util/video/list.dart
@@ -4,6 +4,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
+import 'package:package_info/package_info.dart';
 import 'package:tv_6du/ui/util/menu.dart';
 import 'package:tv_6du/ui/util/video.dart';
 
@@ -16,8 +17,18 @@ class VideoList extends StatefulWidget {
   }
 }
 
+class ListBuilder<T> {
+  final Widget Function(BuildContext, T) builder;
+  final int count;
+
+  const ListBuilder(this.builder, this.count);
+}
+
 class VideoListState extends State<VideoList> {
   int _position = 0;
+  final double padding = 6;
+  final scrollController = ScrollController();
+
   String _url;
   Future<List> fetchVideo() async {
     return (await Dio().get('https://auth.html.ucommuner.com/test.json')).data;
@@ -33,14 +44,44 @@ class VideoListState extends State<VideoList> {
     print("goto $_url");
   }
 
-  @override
-  Widget build(BuildContext context) {
+  Widget Function(BuildContext, AsyncSnapshot<T>) _builder<T>(
+      ListBuilder Function(BuildContext, T) function) {
+    return (context, snapshot) {
+      if (snapshot.hasError) {
+        debugPrint(snapshot.error.toString());
+        return Icon(Icons.error);
+      }
+      if (snapshot.connectionState != ConnectionState.done) {
+        return Column(children: <Widget>[
+          menu(),
+          Expanded(child: Center(child: CircularProgressIndicator()))
+        ]);
+      }
+      ListBuilder o = function(context, snapshot.data);
+      return Scrollbar(
+        child: ListView.builder(
+          padding: EdgeInsets.only(bottom: padding),
+          controller: scrollController,
+          itemCount: 1 + o.count,
+          //itemCount: itemCount,
+          itemBuilder: (context, item) {
+            if (item == 0) return menu();
+            return o.builder(context, item - 1);
+          },
+        ),
+      );
+    };
+  }
+
+  Widget menu() {
+    return Menu(goto, _position);
+  }
+
+  Widget _video() {
     final size = MediaQuery.of(context).size;
     double width = size.width;
     double height = size.height;
-    final scrollController = ScrollController();
     int n;
-    final double padding = 6;
 
     if (height < width) {
       n = 7;
@@ -53,72 +94,69 @@ class VideoListState extends State<VideoList> {
 
     return FutureBuilder<List>(
         future: fetchVideo(),
-        builder: (context, snapshot) {
-          Menu _menu = Menu(goto, _position);
-          if (snapshot.hasError) {
-            debugPrint(snapshot.error.toString());
-            return Icon(Icons.error);
-          }
-          if (snapshot.connectionState != ConnectionState.done) {
-            return Column(children: <Widget>[
-              _menu,
-              Expanded(child: Center(child: CircularProgressIndicator()))
-            ]);
-          }
+        builder: _builder((context, data) {
           List<Widget> children(item) {
             List<Widget> li = <Widget>[];
 
-            switch (_url) {
-              case 'setting':
-                PackageInfo packageInfo = await PackageInfo.fromPlatform();
+            final base = item * n;
+            final end = min<int>(data.length - base, n);
+            for (var i = 0; i < end; ++i) {
+              final o = data[base + i];
 
-                String version = packageInfo.version;
-                String buildNumber = packageInfo.buildNumber;
-                li.add(Focus(
-                    autofocus: true,
-                    child: Text("版本号 $version ($buildNumber)")));
-                break;
-              default:
-                final base = item * n;
-                final end = min(snapshot.data.length - base, n);
-                for (var i = 0; i < end; ++i) {
-                  final o = snapshot.data[base + i];
-
-                  li.add(Padding(
-                      padding: EdgeInsets.all(padding),
-                      child: VideoWidget(scrollController,
-                          img: o[1],
-                          title: o[0],
-                          width: width,
-                          height: height,
-                          padding: padding,
-                          onKey: (FocusNode node, RawKeyEvent event) {
-                        if (event.logicalKey ==
-                            LogicalKeyboardKey(0x10200000004)) {
-                          setState(() {});
-                          return true;
-                        }
-                        return false;
-                      })));
-                }
+              li.add(Padding(
+                  padding: EdgeInsets.all(padding),
+                  child: VideoWidget(scrollController,
+                      img: o[1],
+                      title: o[0],
+                      width: width,
+                      height: height,
+                      padding: padding,
+                      onKey: (FocusNode node, RawKeyEvent event) {
+                    if (event.logicalKey == LogicalKeyboardKey(0x10200000004)) {
+                      setState(() {});
+                      return true;
+                    }
+                    return false;
+                  })));
             }
+
             return li;
           }
 
-          return Scrollbar(
-            child: ListView.builder(
-              padding: EdgeInsets.only(bottom: padding),
-              controller: scrollController,
-              itemCount: 1 + ((snapshot.data.length + n - 1) ~/ n),
-              //itemCount: itemCount,
-              itemBuilder: (context, item) {
-                if (item == 0) return _menu;
-                return Container(
-                    padding: EdgeInsets.fromLTRB(padding, 0, padding, 0),
-                    child: Row(children: children(item - 1)));
-              },
-            ),
+          return ListBuilder(
+            (context, item) {
+              return Container(
+                  padding: EdgeInsets.fromLTRB(padding, 0, padding, 0),
+                  child: Row(children: children(item)));
+            },
+            ((data.length + n - 1) ~/ n),
           );
-        }); // This trailing comma makes auto-formatting nicer for build methods.
+        }));
+  }
+
+  Widget _setting() {
+    /*  
+    String version = packageInfo.version;
+    String buildNumber = packageInfo.buildNumber;
+    return Focus(autofocus: true, child: Text("版本号 $version ($buildNumber)"));
+    */
+    return FutureBuilder<PackageInfo>(
+        future: PackageInfo.fromPlatform(),
+        builder: _builder((context, data) {
+          return ListBuilder((context, item) {
+            return Text(
+                "${data.appName} ${data.packageName} version ${data.version} buildNumber ${data.buildNumber}");
+          }, 1);
+        }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    switch (_url) {
+      case 'setting':
+        return _setting();
+      default:
+        return _video();
+    }
   }
 }
