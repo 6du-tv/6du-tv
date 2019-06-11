714f29b3fa1447f52c629de7c92b1d15a5a5781b
- <->
2019-06-11 18:09:50 +0800

    .

---
+++ release/pypi/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='6du.tv',
+    version='0.0.1',
+    description='6du.tv',
+    author='6du.tv',
+    author_email='eyun@protonmail.com',
+    url='https://github.com/6du-tv/6du-tv',
+    packages=[],
+    packages_data=[
+        "6du.tv.apk"
+    ]
+)
+
