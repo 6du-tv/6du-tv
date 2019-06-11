6f8c0d59aedb46e8dca6e52a2d09af746e665fef
- <->
2019-06-11 17:09:54 +0800

    .

--- github_release.sh
+++ github_release.sh
@@ -3,11 +3,17 @@
 _dirname=$(cd "$(dirname "$0")"; pwd)
 
 cd $_dirname
+
+$version=0.0.1
+git add -u
+git commit -m"."
+
+git tag v$version
 source ./release.sh
 
 github-release upload \
   --owner 6du-tv \
   --repo 6du-tv \
-  --tag "v0.0.1" \
-  --name "v0.0.1" \
+  --tag "v$version" \
+  --name "$version" \
   --body "" $APK
